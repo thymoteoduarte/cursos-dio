8c2df443ec1eb0985fca24cbed2bf9d22bedba5d
thymoteoduarte <thymoteoduarte@gmail.com>
2022-01-26 15:11:59 -0300

    novos pacotes para as aulas de herança - interface

---
+++ introducion-java-ecosystem/src/com/dio/heranca4/Calculadora.java
@@ -0,0 +1,26 @@
+package com.dio.heranca4;
+
+/*
+ * Interface de exemplo para o exercicio da aula de POO
+ **/
+
+public class Calculadora implements OperacaoMatematica{
+	
+	@Override
+	public void soma(double operando1, double operando2) {
+		System.out.println("Soma: "+(operando1+operando2));
+	}
+	@Override
+	public void subtracao(double operando1, double operando2) {
+		System.out.println("Subtração: "+(operando1-operando2));
+	}
+	@Override
+	public void multiplicacao(double operando1, double operando2) {
+		System.out.println("Multiplicação: "+(operando1*operando2));
+	}
+	@Override
+	public void divisao(double operando1, double operando2) {
+		System.out.println("Divisão: "+(operando1/operando2));
+	}
+	
+}
---
+++ introducion-java-ecosystem/src/com/dio/heranca4/OperacaoMatematica.java
@@ -0,0 +1,18 @@
+package com.dio.heranca4;
+
+/*
+ * Interface de exemplo para o exercicio da aula de POO
+ **/
+
+public interface OperacaoMatematica {
+	
+	void soma(double operando1, double operando2);
+	
+	void subtracao(double operando1, double operando2);
+	
+	void divisao(double operando1, double operando2);
+	
+	void multiplicacao(double operando1, double operando2);
+	
+
+}
