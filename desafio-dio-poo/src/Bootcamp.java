1ae30f682b6bba0cd30f8316d6465f1cf90ee596
thymoteo <thymoteoduarte@gmail.com>
2022-02-02 21:58:03 -0300

    novas implementações do desafio

---
+++ desafio-dio-poo/src/Bootcamp.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+	
+	private String nome;
+	private String descricao;
+	private final LocalDate dataInicial = LocalDate.now();
+	private final LocalDate dataFinal = dataInicial.plusDays(45);
+	private Set<Dev> devsInscritos = new HashSet<>();
+	private Set<Conteudo> conteudos = new LinkedHashSet()<>();
+}
---
+++ desafio-dio-poo/src/Dev.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+	
+	private String nome;
+	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+	public void inscreverBootcamp(Bootcamp bootcamp){};
+	
+	public void progredir(){};
+	
+	public void calcularTotalXp(){};
+}
