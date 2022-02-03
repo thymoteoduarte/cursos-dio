10faa53521002396c7f0650fcc19e62dab833729
thymoteo <thymoteoduarte@gmail.com>
2022-02-02 21:42:45 -0300

    novas implementações do desafio

--- desafio-dio-poo/src/Curso.java
+++ desafio-dio-poo/src/Curso.java
@@ -1,31 +1,28 @@
 
-public class Curso {
-	
-	private String titulo;
-	private String descricao;
+public class Curso extends Conteudo{
+
 	private int cargaHoraria;
+
 	
-	public String getTitulo() {
-		return titulo;
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO * cargaHoraria;
 	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+	
+	public Curso() {
+		
+	}	
+	
 	public int getCargaHoraria() {
 		return cargaHoraria;
 	}
+	
 	public void setCargaHoraria(int cargaHoraria) {
 		this.cargaHoraria = cargaHoraria;
 	}
 	@Override
 	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
 	}
 
 }
--- desafio-dio-poo/src/Mentoria.java
+++ desafio-dio-poo/src/Mentoria.java
@@ -1,22 +1,16 @@
 import java.time.LocalDate;
 
-public class Mentoria {
-	
-	private String titulo;
-	private String descricao;
+public class Mentoria extends Conteudo{
+
 	private LocalDate data;
 	
-	public String getTitulo() {
-		return titulo;
+	public Mentoria() {
+
 	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO + 20d;
 	}
 	public LocalDate getData() {
 		return data;
@@ -26,7 +20,7 @@ public class Mentoria {
 	}
 	@Override
 	public String toString() {
-		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
 	}
 
 }
